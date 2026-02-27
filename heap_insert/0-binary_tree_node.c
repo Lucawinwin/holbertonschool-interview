9ddf5ab721841a19b427273b73a4f713a4f5616c
lucawinwin <windwehrluca@gmail.com>
2026-02-27 09:42:18 +0100

    upt task 0

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,17 +1,18 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer which point adress parent
- * @value: value of the node
+ * binary_tree_node - Create a new binary tree node
+ * @parent: Pointer to the parent of the node to create
+ * @value: Integer value to store in the new node
  *
- * Return: the create the three node
+ * Return: Pointer to the new node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
+	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 
